bd72fc2a06b8007fb6ad901ba4c28240c6fff45b
kavalar <apuc06@mail.ru>
2020-12-16 11:45:54 +0300

    update

--- include/ArgsParse.py
+++ include/ArgsParse.py
@@ -6,8 +6,9 @@ class ArgsParser:
     def parse():
         parser = ArgumentParser()
 
-        parser.add_argument('-r', '--region', dest='Region id', type=int, default=1,
-                            help='Идентификатор региона')
+        parser.add_argument('-g', '--geo', dest='Geo id', type=int, default=1, help='Region id')
+
+        parser.add_argument('-r', '--rubric', dest='Rubric id', type=int, default=1, help='Rubric id')
 
         args = parser.parse_args()
         return args
--- main.py
+++ main.py
@@ -11,31 +11,68 @@ import json
 
 if __name__ == '__main__':
     args = ArgsParser.parse()
-    #res = requests.get('https://rostovskaya-oblast.zarplata.ru/api/v1/companies/18681793')
-    # with open('./data_samples/beeline.json') as f:
-    #     company = json.loads(f.read())
-
-    # phone = PhoneFormat(phone='88002500250', tpl='+$ $$$ $$$-$$-$$')
-
-    api = ZpApi('rostovskaya-oblast')
-    res = api.do_request('vacancies', 204506901)
-
-    # hash = PasswordGen.password_hash('123edsaqw'.encode("utf-8"))
-    # Some.test()
-    # Some_2.my()
-
-    # db = BaseModel()
-    # print(123)
-    vacancy_dto = VacancyDTO(res['vacancies'][0])
-    user_dto = UserDTO(res['companies'][0])
-
-    user = UserModel.crate_user(user_dto)
-
-    employer = EmployerModel.create_employer(user, user_dto)
-
-    company = CompanyModel.create_company(user, user_dto)
-
-    phone = PhoneModel.create_phone(user_dto.phone, user, company)
+    api = ZpApi()
 
 
-    print(f'{args.base_region_url}')
+    # geo_count = api.do_geo_request(limit=0, offset=0)['metadata']['resultset']['count']
+    # for counter in range(0, geo_count, 100):
+    #     geo_res = api.do_geo_request(limit=100, offset=counter)
+    #     for geo_item in geo_res['geo']:
+    #         rubric_res = api.do_rubric_request()['rubrics']
+    #         for rubric in rubric_res:
+    #             vacancy_count = api.do_vacancy_request(geo_id=geo_item['id'], rubric_id=rubric['id'], limit=0, offset=0)['metadata']['resultset']['count']
+    #             company_list = []
+    #             for counter in range(0, vacancy_count, 100):
+    #                 vacancy_res = api.do_vacancy_request(geo_id=geo_item['id'], rubric_id=rubric['id'], limit=100, offset=counter)
+    #                 for vacancy in vacancy_res['vacancies']:
+    #                     vacancy_dto = VacancyDTO(vacancy)
+    #                     if vacancy_dto.owner not in company_list:
+    #                         company_res = api.do_company_request(company_id=vacancy_dto.owner)
+    #                         user_dto = UserDTO(company_res['companies'][0])
+    #                         company_list.append(vacancy_dto.owner)
+    #                         company_list.append(vacancy_dto.owner)
+    #                         user_insert = UserModel.crate_user(user_dto)
+    #                         employer_insert = EmployerModel.create_employer(user_insert, user_dto)
+    #                         company_insert = CompanyModel.create_company(user_insert, user_dto)
+    #                         phone_insert = PhoneModel.create_phone(user_dto.phone, user_insert, company_insert, employer_insert)
+    #                     vacancy_insert = VacancyModel.create_vacancy(vacancy_dto)
+
+    #TODO: add proxies, args
+    
+    vacancy_count = api.do_vacancy_request(geo_id=61, rubric_id=138, limit=0, offset=0)['metadata']['resultset']['count']
+    company_list = []
+    for counter in range(0, vacancy_count, 100):
+        vacancy_res = api.do_vacancy_request(geo_id=61, rubric_id=138, limit=100, offset=counter)
+        with db_handle.atomic():
+            for vacancy in vacancy_res['vacancies']:
+                vacancy_dto = VacancyDTO(vacancy)
+                if vacancy_dto.owner not in company_list:
+                    company_res = api.do_company_request(company_id=vacancy_dto.owner)
+                    user_dto = UserDTO(company_res['companies'][0])
+                    company_list.append(vacancy_dto.owner)
+                    user_insert = UserModel.crate_user(user_dto)
+                    employer_insert = EmployerModel.create_employer(user_insert, user_dto)
+                    company_insert = CompanyModel.create_company(user_insert, user_dto)
+                    phone_insert = PhoneModel.create_phone(user_dto.phone, user_insert, company_insert, employer_insert)
+                vacancy_insert = VacancyModel.create_vacancy(vacancy_dto)
+            db_handle.commit()
+            
+            
+
+    point = 'test'
+
+
+
+    # vacancy_dto = VacancyDTO(res['vacancies'][0])
+    # user_dto = UserDTO(res['companies'][0])
+
+    # user = UserModel.crate_user(user_dto)
+
+    # employer = EmployerModel.create_employer(user, user_dto)
+
+    # company = CompanyModel.create_company(user, user_dto)
+
+    # phone = PhoneModel.create_phone(user_dto.phone, user, company)
+
+
+    # print(f'{args.base_region_url}')
